67155ddea4e4854b9fdac0b040c5f3d737a3c8c0
YedixonRDev <yedixonjrf@gmail.com>
2024-08-19 20:14:40 -0500

    Primera version del juego

---
+++ script.js
@@ -0,0 +1,37 @@
+// Función para los botones
+document.getElementById("piedra").addEventListener("click", function () {
+  jugar("piedra");
+});
+document.getElementById("papel").addEventListener("click", function () {
+  jugar("papel");
+});
+document.getElementById("tijera").addEventListener("click", function () {
+  jugar("tijera");
+});
+
+// Función para jugar
+function jugar(eleccionUsuario) {
+  const opciones = ["piedra", "papel", "tijera"];
+  const eleccionPc = opciones[Math.floor(Math.random() * 3)];
+
+  let resultado = "";
+
+  if (eleccionUsuario === eleccionPc) {
+    resultado = "¡Empate!";
+  } else if (
+    (eleccionUsuario === "piedra" && eleccionPc === "tijera") ||
+    (eleccionUsuario === "tijera" && eleccionPc === "papel") ||
+    (eleccionUsuario === "papel" && eleccionPc === "piedra")
+  ) {
+    resultado = `¡Ganastes! Elegiste ${eleccionUsuario} y la computadora eligió ${eleccionPc}.`;
+  } else {
+    resultado = `¡Perdiste! Elegiste ${eleccionUsuario} y la computadora eligió ${eleccionPc}.`;
+  }
+
+  document.getElementById("resultado").textContent = resultado;
+}
+
+// Función para vovler a jugar
+function repetir() {
+  document.getElementById("resultado").textContent = "";
+}
